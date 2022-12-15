9e26cac3ab0c89c111748d30fe2fc478daa093f1
OPaslavskiy <bankir.op@gmail.com>
2022-12-15 14:42:34 +0200

    task 02_ ready

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', currentTime);
+
+function currentTime(evt) {
+  throttle(localStorage.setItem('videoplayer-current-time', evt.seconds), 1000);
+}
+
+try {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+} catch (error) {
+  player.setCurrentTime(0);
+}
