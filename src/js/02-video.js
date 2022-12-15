e109a10d1a37d83c22816400527a040407570708
OPaslavskiy <bankir.op@gmail.com>
2022-12-15 19:26:42 +0200

    Update 02tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,18 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const TIME_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 player.on('timeupdate', currentTime);
 
 function currentTime(evt) {
-  throttle(localStorage.setItem('videoplayer-current-time', evt.seconds), 1000);
+  throttle(localStorage.setItem(TIME_KEY, evt.seconds), 1000);
 }
 
 try {
-  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+  player.setCurrentTime(localStorage.getItem(TIME_KEY));
 } catch (error) {
   player.setCurrentTime(0);
 }
