82b6efda52679a6befa0b431b1a47c445f1b0058
OPaslavskiy <bankir.op@gmail.com>
2022-12-17 10:59:23 +0200

    ready_03_Task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
+const submitForm = document.querySelector('.feedback-form');
+const textArea = document.querySelector('textarea');
 
+const MESSAGE_KEY = 'userMessage';
+
+submitForm.addEventListener('submit', onFormSubmit);
+textArea.addEventListener('input', throttle(enteringMessage, 500));
+
+saveTextarea();
+
+function enteringMessage(event) {
+  const message = event.target.value;
+  localStorage.setItem(MESSAGE_KEY, message);
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElement = event.currentTarget.elements;
+
+  if (!formElement.email.value || !formElement.message.value) {
+    alert('Dude, fill in all the fields');
+  }
+  const formData = new FormData(event.currentTarget);
+
+  console.log(formData);
+  if (formElement.email.value && formElement.message.value) {
+    submitForm.reset();
+    localStorage.removeItem(MESSAGE_KEY);
+  }
+}
+
+function saveTextarea() {
+  const savedMessage = localStorage.getItem(MESSAGE_KEY);
+  if (savedMessage) {
+    textArea.value = savedMessage;
+  }
+}
